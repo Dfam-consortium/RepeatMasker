477a478673d175b37dc6bd3b189e4d96f08b7dc7
Robert Hubley <rhubley@systemsbiology.org>
2020-06-18 14:44:38 -0700

    - Getting ready to push

--- RepeatMaskerConfig.pm
+++ RepeatMaskerConfig.pm
@@ -66,7 +66,7 @@ BEGIN {
                              'expected_files' => [],
                              'param_type' => 'directory',
                              'required' => 0,
-                             'value' => '/usr/local/abblast'
+                             'value' => ''
                            },
           'CROSSMATCH_DIR' => {
                                 'command_line_override' => 'crossmatch_dir',
@@ -78,14 +78,14 @@ BEGIN {
                                 'expected_files' => [],
                                 'param_type' => 'directory',
                                 'required' => 0,
-                                'value' => '/usr/local/phrap'
+                                'value' => ''
                               },
           'DEFAULT_SEARCH_ENGINE' => {
                                        'command_line_override' => 'default_search_engine',
                                        'description' => 'The default search engine to use',
                                        'param_type' => 'value',
                                        'required' => 1,
-                                       'value' => 'rmblast'
+                                       'value' => ''
                                      },
           'HMMER_DIR' => {
                            'command_line_override' => 'hmmer_dir',
@@ -97,7 +97,7 @@ BEGIN {
                            'expected_files' => [],
                            'param_type' => 'directory',
                            'required' => 0,
-                           'value' => '/usr/local/hmmer/bin'
+                           'value' => ''
                          },
           'LIBDIR' => {
                         'command_line_override' => 'libdir',
@@ -115,7 +115,7 @@ BEGIN {
                                             ],
                         'param_type' => 'directory',
                         'required' => 0,
-                        'value' => '/u1/home/rhubley/projects/RepeatMasker/Libraries'
+                        'value' => ''
                       },
           'RMBLAST_DIR' => {
                              'command_line_override' => 'rmblast_dir',
@@ -132,7 +132,7 @@ BEGIN {
                              'expected_files' => [],
                              'param_type' => 'directory',
                              'required' => 0,
-                             'value' => '/usr/local/rmblast-2.10.0/bin'
+                             'value' => ''
                            },
           'TRF_PRGM' => {
                           'command_line_override' => 'trf_prgm',
@@ -140,7 +140,7 @@ BEGIN {
                           'environment_override' => 'TRF_PRGM',
                           'param_type' => 'program',
                           'required' => 1,
-                          'value' => '/usr/local/bin/trf409.linux64'
+                          'value' => ''
                         }
         };
 
