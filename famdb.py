beecc4a4ec5fffc9af35f17d7c23533a6f4340b3
Jeb Rosen <jeb.rosen@isbscience.org>
2021-03-30 13:44:20 -0700

    - Update famdb.py to 0.4.2: bugfix for importing Refineable families in EMBL files

--- famdb.py
+++ famdb.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-    famdb.py, version 0.4.1
+    famdb.py, version 0.4.2
     Usage: famdb.py [-h] [-l LOG_LEVEL] [-i FILE] command ...
 
     Queries or modifies the contents of a famdb file. For more detailed help
@@ -585,6 +585,8 @@ class Family:  # pylint: disable=too-many-instance-attributes
             elif code == "DE":
                 family.description = value
             elif code == "CC":
+                # TODO: Consider only recognizing these after seeing "RepeatMasker Annotations"
+
                 matches = re.match(r'\s*Type:\s*(\S+)', value)
                 if matches:
                     family.repeat_type = matches.group(1).strip()
@@ -611,6 +613,10 @@ class Family:  # pylint: disable=too-many-instance-attributes
                 matches = re.search(r'BufferStages:\s*(\S+)', value)
                 if matches:
                     family.buffer_stages = matches.group(1).strip()
+
+                matches = re.search(r'Refineable', value)
+                if matches:
+                    family.refineable = True
 
         header = ""
         family = None
@@ -728,7 +734,7 @@ class FamDB:
             self.names_dump = json.loads(self.file["TaxaNames"][0])
 
     def __write_metadata(self):
-        self.file.attrs["generator"] = "famdb.py v0.1"
+        self.file.attrs["generator"] = "famdb.py v0.4.2"
         self.file.attrs["version"] = FILE_VERSION
         self.file.attrs["created"] = str(datetime.datetime.now())
 
@@ -1673,7 +1679,7 @@ def main():
 
     logging.basicConfig()
 
-    parser = argparse.ArgumentParser(description="""This is famdb.py version 0.4.1.
+    parser = argparse.ArgumentParser(description="""This is famdb.py version 0.4.2.
 
 example commands, including the most commonly used options:
 
