a473c13ef441426b64f19d18a2784523273f44d4
Robert Hubley <rhubley@systemsbiology.org>
2024-09-10 16:40:38 -0700

    - Fixes for Dfam 3.8 famdb

--- famdb_helper_classes.py
+++ famdb_helper_classes.py
@@ -632,13 +632,17 @@ class Family:  # pylint: disable=too-many-instance-attributes
                     for spec in matches.group(1).split(","):
                         name = spec.strip()
                         if name:
-                            name = name.replace("[", "")
-                            name = name.replace("]", "")
-                            tax_id = lookup.get(name.lower())
+                            tax_id = lookup.get(name)
                             if tax_id is not None:
                                 family.clades += [tax_id]
                             else:
-                                LOGGER.warning("Could not find taxon for '%s'", name)
+                                name = name.replace("[", "")
+                                name = name.replace("]", "")
+                                tax_id = lookup.get(name.lower())
+                                if tax_id is not None:
+                                    family.clades += [tax_id]
+                                else:
+                                    LOGGER.warning("Could not find taxon for '%s' upper or lower: line=%s, and ID=%s", name, value, family.accession)
                 matches = re.search(r"SearchStages:\s*(\S+)", value)
                 if matches:
                     family.search_stages = matches.group(1).strip()
